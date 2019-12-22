c91c80d27716057c6b7846d7ceaabee5f19330d0
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-12-22 17:44:41 +0300

    update app

--- src/index.js
+++ src/index.js
@@ -1,47 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// import { SeasonDisplay } from "./seasonDisplay";
-
-// const App = () => {
-//   window.navigator.geolocation.getCurrentPosition(
-//     position => console.log(position),
-//     err => console.log(err)
-//   );
-//   return (
-//     <div>
-//       Hello!
-//       <SeasonDisplay />
-//     </div>
-//   );
-// };
+import { SeasonDisplay } from "./seasonDisplay";
+import { Spinner } from "./loader";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { latitude: null, errorMessage: "" };
-  }
-
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { latitude: null, errorMessage: "" };
+  // }
+  state = { latitude: null, errorMessage: "" };
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ latitude: position.coords.latitude }),
       err => this.setState({ errorMessage: err.message })
     );
   }
-
-  // componentDidUpdate(prevProps, prevState, snapshot) {
-  //   console.log("my component was updated!");
-  // }
-
-  render() {
-    console.log("rendered");
+  renderBody() {
     if (this.state.errorMessage && !this.state.latitude) {
       return <div>Error: {this.state.errorMessage}</div>;
     } else if (!this.state.errorMessage && this.state.latitude) {
-      return <div>Latitude: {this.state.latitude} </div>;
+      return <SeasonDisplay lat={this.state.latitude} />;
     } else {
-      return <div>Loading... </div>;
+      return <Spinner message={"Please accept location request"} />;
     }
+  }
+
+  render() {
+    return <div className={"border red"}>{this.renderBody()}</div>;
   }
 }
 
