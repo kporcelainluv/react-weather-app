a1f913a2d810d3678de2d60128f84d2cebabea97
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-12-22 13:40:41 +0300

    add componentDidMount

--- src/index.js
+++ src/index.js
@@ -17,32 +17,21 @@ import ReactDOM from "react-dom";
 // };
 
 class App extends React.Component {
-  // being called first so its perfect for initializing state
   constructor(props) {
-    // as we extend it from react component, we must inherit
     super(props);
-    // if value is a number one uses null
     this.state = { latitude: null, errorMessage: "" };
-
-    window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ latitude: position.coords.latitude });
-      },
-
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
-    );
   }
-  // render must be in class component
 
   componentDidMount() {
-    console.log("component was rendered to the screen");
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ latitude: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
   }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log("my component was updated!");
-  }
+  // componentDidUpdate(prevProps, prevState, snapshot) {
+  //   console.log("my component was updated!");
+  // }
 
   render() {
     console.log("rendered");
