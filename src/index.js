8010a2d0e29379ed297d2f6356f8a77ff0962f81
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-12-22 12:06:55 +0300

    wip

--- src/index.js
+++ src/index.js
@@ -17,13 +17,21 @@ import { SeasonDisplay } from "./seasonDisplay";
 // };
 
 class App extends React.Component {
+  // being called first so its perfect for initializing state
+  constructor(props) {
+    // as we extend it from react component, we must inherit
+    super(props);
+    // if value is a number one uses null
+    this.state = { latitude: null };
+  }
+  // render must be in class component
   render() {
     window.navigator.geolocation.getCurrentPosition(
       position => console.log(position),
       err => console.log(err)
     );
 
-    return <div>Latitude: </div>;
+    return <div>Latitude: {this.state.latitude}</div>;
   }
 }
 
